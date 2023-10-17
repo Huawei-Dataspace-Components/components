8ebeab4374cfc1af45235be337de918761110f46
Enrico Risa <enrico.risa@gmail.com>
2023-10-17 21:19:06 +0200

    fix tests

--- e2e-tests/build.gradle.kts
+++ e2e-tests/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     testImplementation(libs.awaitility)
     testImplementation(libs.testcontainers.junit)
     testImplementation(testFixtures(project(":extensions:common:obs:obs-core")))
+    testCompileOnly(project(":launchers"))
 }
 
 // do not publish
