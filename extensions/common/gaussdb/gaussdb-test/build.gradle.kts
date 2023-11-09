f8e9af161d082b9aba89187da4412dbf2965fdee
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2023-11-09 14:06:41 +0100

    feat: implement GaussDB Asset Index

---
+++ extensions/common/gaussdb/gaussdb-test/build.gradle.kts
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2020, 2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+plugins {
+    `java-library`
+    `java-test-fixtures`
+    `maven-publish`
+}
+
+dependencies {
+    api(libs.edc.controlplane.spi)
+    api(libs.huawei.dws.jdbc)
+//    api(files("/Users/paullatzelsperger/Downloads/gsjdbc4.jar"))
+    testFixturesApi(libs.testcontainers.junit)
+
+    testFixturesApi(libs.edc.util)
+    testFixturesApi(libs.edc.junit)
+    testFixturesApi(libs.edc.sql.core)
+    testFixturesApi(libs.junit.jupiter.api)
+}
+
+
