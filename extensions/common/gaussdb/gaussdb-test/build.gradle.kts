03b703edeccc19770b9ed12401bd5a1c1caa6301
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2023-11-09 17:34:18 +0100

    removed wrong lic headers

--- extensions/common/gaussdb/gaussdb-test/build.gradle.kts
+++ extensions/common/gaussdb/gaussdb-test/build.gradle.kts
@@ -1,17 +1,3 @@
-/*
- *  Copyright (c) 2020, 2021 Microsoft Corporation
- *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
- *
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Contributors:
- *       Microsoft Corporation - initial API and implementation
- *
- */
-
 plugins {
     `java-library`
     `java-test-fixtures`
@@ -21,7 +7,6 @@ plugins {
 dependencies {
     api(libs.edc.controlplane.spi)
     api(libs.huawei.dws.jdbc)
-//    api(files("/Users/paullatzelsperger/Downloads/gsjdbc4.jar"))
     testFixturesApi(libs.testcontainers.junit)
 
     testFixturesApi(libs.edc.util)
--- extensions/common/gaussdb/gaussdb-test/src/testFixtures/java/org/eclipse/edc/azure/testfixtures/GaussDbPostgresFunctions.java
+++ extensions/common/gaussdb/gaussdb-test/src/testFixtures/java/org/eclipse/edc/azure/testfixtures/GaussDbPostgresFunctions.java
@@ -1,17 +1,3 @@
-/*
- *  Copyright (c) 2023 Bayerische Motorenwerke Aktiengesellschaft
- *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
- *
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Contributors:
- *       Bayerische Motorenwerke Aktiengesellschaft - initial API and implementation
- *
- */
-
 package org.eclipse.edc.azure.testfixtures;
 
 import org.jetbrains.annotations.NotNull;
